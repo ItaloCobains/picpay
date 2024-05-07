033cb4e24ebb7d917a769efd0c519196642a66b7
ItaloCobains <italobrandao.contato@gmail.com>
2024-05-07 14:25:36 -0300

    Update code formatting and fix minor bugs

---
+++ src/main/java/picpay/desafio/picpay/domain/user/UserType.java
@@ -0,0 +1,6 @@
+package picpay.desafio.picpay.domain.user;
+
+public enum UserType {
+    COMMON,
+    MERCHANT
+}
---
+++ src/main/java/picpay/desafio/picpay/repositories/UserRepository.java
@@ -0,0 +1,12 @@
+package picpay.desafio.picpay.repositories;
+
+import java.util.Optional;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import picpay.desafio.picpay.domain.user.User;
+
+public interface UserRepository extends JpaRepository<User, Long> {
+    Optional<User> findUserByDocument(String document);
+    Optional<User> findUserById(Long id);
+}
